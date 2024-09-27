21bcd548a1a1699ded273a62f8925a87d25e93ae
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-09-27 13:22:54 +0545

    learned about variables

---
+++ variable.js
@@ -0,0 +1,17 @@
+//ES5
+var person = "Ankit Sunuwar"; // Global Variable
+
+//ES6  Keyword haru yesko fixed hunxa var,let,const
+// Always start letter with const
+const anotherPerson = "Ankita";
+let thirdPerson = "Kabita";
+
+console.log(person, anotherPerson, thirdPerson);
+
+var test = "Asd";
+var test = "fgh";
+console.log(test);
+
+let newPerson = "tiger";
+newPerson = "Lion";
+console.log(newPerson);
